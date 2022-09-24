5b4bc576d1e1e0e5d0a828e106b0f86b64bf9550
SabrinaOC <sabriojea@gmail.com>
2022-09-24 21:50:19 +0200

    feat(calc): division

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -6,10 +6,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.component.scss']
 })
 export class CalculadoraComponent implements OnInit {
+  primerValor: string = '';
+  segundoValor: string = '';
+  operation: string = ''; // 0: division, 1: multiplicacion, 2: suma, 3: resta; -1: clear; 10: igual
+  result!: number | null;
+  isPrimerValueOperatorInserted: boolean = false;
+  isError: boolean = false;
+  errorMessage!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * 
+   * @param valor 
+   */
+  getAndAddValue(valor: string) {
+    //comprobamos si añadimos a primer valor o a segundo
+    if(!this.isPrimerValueOperatorInserted) {
+      this.primerValor += valor;
+    } else {
+      this.segundoValor += valor;
+    }
+  }
+
+  /**
+   * 
+   */
+  getOperator(operador: string) {
+    this.operation = operador;
+    console.log('Operador: ', this.operation);
+    //ponemos bandera de que se ha terminado de introducir el primer valor
+    this.isPrimerValueOperatorInserted = true;
+  }
+
+  getResult() {
+    //identificamos operacion
+    if(this.checkErrors()){
+      switch(this.operation) {
+      case 'C':
+        //reseteamos todo
+        this.primerValor = '';
+        break;
+      case '/':
+        this.result = parseFloat(this.primerValor)/parseFloat(this.segundoValor);
+        console.log('Resultado: ', this.result)
+        break;
+      }
+      //asignamos resultado a primer valor para poder concatenar operaciones y reseteamos res y segundoVal
+      this.primerValor = '' + this.result;
+      this.result = null;
+      this.segundoValor = '';
+      this.operation = '';
+    }
+  }
+
+  /**
+   * 
+   * @returns 
+   */
+  checkErrors() {
+    console.log('Entra en check errors')
+    //comprobamos que tenemos valores para obtener resultado
+    if(this.primerValor === '') {
+      this.isError = true;
+      this.errorMessage = 'No value';
+      console.log('Error ', this.errorMessage)
+      return false;
+    }
+
+    return true;
   }
 
 }
